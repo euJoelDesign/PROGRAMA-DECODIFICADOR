c230561df7e5dc692492c8ddc918a515a782368d
euJoelDesign <158786704+euJoelDesign@users.noreply.github.com>
2024-02-07 11:22:27 -0300

    Update descriptor.js

--- js/descriptor.js
+++ js/descriptor.js
@@ -4,8 +4,8 @@ var outCaixa = document.querySelector("#exibe-mensagem");
 function criptoText(){
     var texto = textMain.value;
 
-    var cripResult = texto.replace(/a/g, "ai").replace(/e/g, "enter")
-    .replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+    var cripResult = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober")
+    .replace(/a/g, "ai").replace(/u/g, "ufat");
     
     document.getElementById('exibe-mensagem').innerHTML ='<textarea readonly class="copy" id="msgCampo">' + cripResult +
     '</textarea>'
@@ -18,8 +18,7 @@ function criptoText(){
 function descriptoText(){
     var texto = textMain.value;
 
-    var descResult = texto.replace(/ai/g, "a") .replace(/enter/g, "e")
-    .replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+    var descResult = texto.replace(/ai/g,"a").replace(/enter/g,"e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g,"u");
     
     document.getElementById('exibe-mensagem').innerHTML ='<textarea readonly class="copy" id="msgCampo">' + descResult +
     '</textarea>' 
